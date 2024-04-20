17252efa91e3b3819b85ed4d809727eac3b077a4
Elena Rangelov <elenarangelov04@gmail.com>
2024-04-20 18:04:30 -0400

    ALSKJDFLKSJDFLKSJADF

--- login_backend.py
+++ login_backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,14 +8,11 @@ def home():
     redirect_uri = 'http://localhost:5000/oauth2callback'  # Replace with your actual redirect URI
     scope = 'https://www.googleapis.com/auth/calendar'
     oauth_link = f"https://accounts.google.com/o/oauth2/auth?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope={scope}&access_type=offline&prompt=consent"
-    return f"<a href='{oauth_link}'>Click here to authorize with Google</a>"
+    return render_template('login.html', oauth_link=oauth_link)
 
 @app.route('/oauth2callback')
 def callback():
-    # This route will be called by Google after the user authorizes your app
     code = request.args.get('code')
-    # You can now exchange this code for an access token and refresh token
-    # Implement the token exchange logic here
     return f"Authorization code received: {code}"
 
 if __name__ == '__main__':
